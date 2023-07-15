b4ddc894284ab69702836038d41f8edcee850eef
Pratik Sarkar <pratiksarkar.573@gmail.com>
2023-07-15 09:09:35 +0530

    Sentry Logging added

--- react-error-and-logging/error_and_logging/src/main.jsx
+++ react-error-and-logging/error_and_logging/src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://6f01a29a38674803861d74584024f57c@o4505527202545664.ingest.sentry.io/4505527672635392",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", "https:yourserver.io/api/"],
+    }),
+    new Sentry.Replay({
+      maskAllInputs: false,
+      maskAllText: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 0.1, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+  beforeSend: (event, hint) => {
+    console.log("Going to sentry", event, "---", hint);
+    return event;
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- react-error-and-logging/error_and_logging/vite.config.js
+++ react-error-and-logging/error_and_logging/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // build: {
+  //   sourcemap: true, // Source map generation must be turned on
+  // },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "Test",
+      project: "sentry-logging-learning",
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken:
+        "b73cfe2814e04306aeea8357de6a606c3a4b835d6ba043a6b133d605a0234ea8",
+      sourcemaps: {
+        assets: "./**",
+        ignore: ["./node_modules/**", "./vite.config.js/**"],
+      },
+    }),
+  ],
+});
